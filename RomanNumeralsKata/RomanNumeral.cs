a178ee0f5e0c15b4e2c1fcfad30ee44eb93131fd
Kevin Kodjo <kodjocod@gmail.com>
2018-09-26 15:53:04 +0200

    refacto Dictonary RomanToArabic to deal with number like 43

--- RomanNumeralsKata/RomanNumeral.cs
+++ RomanNumeralsKata/RomanNumeral.cs
@@ -21,12 +21,12 @@ namespace RomanNumeralsKata
 
         private readonly Dictionary<string, int> MatchRomanSymbolsWithArabicNumbers = new Dictionary<string, int>
         {
-            {"L",50 },
             {"XL",40 },
+            { "L",50 },
             { "X", 10},
             {"IX", 9},
-            {"V", 5},
             {"IV", 4},
+            {"V", 5},
             {"I", 1}
         };
         public string ConvertToRomanNumerals(int arabicNumeral)
@@ -54,10 +54,11 @@ namespace RomanNumeralsKata
             {
                 foreach (var romanNumeral in MatchRomanSymbolsWithArabicNumbers.Keys)
                 {
-                    if (romanSymbol.Contains(romanNumeral))
+                    if(romanSymbol.Contains(romanNumeral))
                     {
                         convertedNumber += MatchRomanSymbolsWithArabicNumbers[romanNumeral];
-                        var posRomanSymbolToDelete = romanSymbol.IndexOf(romanNumeral);
+                        int posRomanSymbolToDelete = romanSymbol.IndexOf(romanNumeral, StringComparison.Ordinal);
+
                         romanSymbol = romanSymbol.Remove(posRomanSymbolToDelete,romanNumeral.Length);
                     }
 
--- XUnitTest/ConvertToArabicNumbersShould.cs
+++ XUnitTest/ConvertToArabicNumbersShould.cs
@@ -13,43 +13,10 @@ namespace RomanNumeralKataUnitTest
         public void return_1_when_the_given_roman_symbol_is_I()
         {
             string romanSymbol="I";
-            RomanNumeral romanNumeral = new RomanNumeral();
-            string romanSymbol1 = romanSymbol;
-            int convertedNumber1 = 0;
-            while (romanSymbol1.Length != 0)
-            {
-                var positionOfRomanSymbolToDelete = 0;
+            RomanNumeral romanNumeral = new RomanNumeral();       
 
-                if (romanSymbol1.Contains("X"))
-                {
-                    convertedNumber1 += 10;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("X");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 1);
-                }
-                if (romanSymbol1.Contains("IV"))
-                {
-                    convertedNumber1 += 4;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("IV");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 2);
-                }
-                if (romanSymbol1.Contains("V"))
-                {
-                    convertedNumber1 += 5;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("V");
-                    romanSymbol1= romanSymbol1.Remove(positionOfRomanSymbolToDelete,1);
-                }
+            int convertedNumber= romanNumeral.ConvertToArabicNumber(romanSymbol);
 
-
-                if (romanSymbol1.Contains("I"))
-                {
-                    convertedNumber1 += 1;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("I");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 1);
-                }
-            }
-
-            romanNumeral.ConvertToArabicNumber(romanSymbol1);
-            int convertedNumber= convertedNumber1;
             Check.That(convertedNumber).IsEqualTo(1);
         }
         [Fact]
@@ -57,42 +24,9 @@ namespace RomanNumeralKataUnitTest
         {
             string romanSymbol = "III";
             RomanNumeral romanNumeral = new RomanNumeral();
-            string romanSymbol1 = romanSymbol;
-            int convertedNumber1 = 0;
-            while (romanSymbol1.Length != 0)
-            {
-                var positionOfRomanSymbolToDelete = 0;
+      
+            int convertedNumber = romanNumeral.ConvertToArabicNumber(romanSymbol); 
 
-                if (romanSymbol1.Contains("X"))
-                {
-                    convertedNumber1 += 10;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("X");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 1);
-                }
-                if (romanSymbol1.Contains("IV"))
-                {
-                    convertedNumber1 += 4;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("IV");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 2);
-                }
-                if (romanSymbol1.Contains("V"))
-                {
-                    convertedNumber1 += 5;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("V");
-                    romanSymbol1= romanSymbol1.Remove(positionOfRomanSymbolToDelete,1);
-                }
-
-
-                if (romanSymbol1.Contains("I"))
-                {
-                    convertedNumber1 += 1;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("I");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 1);
-                }
-            }
-
-            romanNumeral.ConvertToArabicNumber(romanSymbol1);
-            int convertedNumber = convertedNumber1;
             Check.That(convertedNumber).IsEqualTo(3);
         }
         [Fact]
@@ -101,42 +35,8 @@ namespace RomanNumeralKataUnitTest
             string romanSymbol = "IV";
             RomanNumeral romanNumeral = new RomanNumeral();
 
-            string romanSymbol1 = romanSymbol;
-            int convertedNumber1 = 0;
-            while (romanSymbol1.Length != 0)
-            {
-                var positionOfRomanSymbolToDelete = 0;
-
-                if (romanSymbol1.Contains("X"))
-                {
-                    convertedNumber1 += 10;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("X");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 1);
-                }
-                if (romanSymbol1.Contains("IV"))
-                {
-                    convertedNumber1 += 4;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("IV");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 2);
-                }
-                if (romanSymbol1.Contains("V"))
-                {
-                    convertedNumber1 += 5;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("V");
-                    romanSymbol1= romanSymbol1.Remove(positionOfRomanSymbolToDelete,1);
-                }
-
-
-                if (romanSymbol1.Contains("I"))
-                {
-                    convertedNumber1 += 1;
-                    positionOfRomanSymbolToDelete = romanSymbol1.IndexOf("I");
-                    romanSymbol1 = romanSymbol1.Remove(positionOfRomanSymbolToDelete, 1);
-                }
-            }
-
-            romanNumeral.ConvertToArabicNumber(romanSymbol1);
-            int convertedNumber = convertedNumber1;
+           
+            int convertedNumber = romanNumeral.ConvertToArabicNumber(romanSymbol); ;
 
             Check.That(convertedNumber).IsEqualTo(4);
         }
@@ -161,16 +61,16 @@ namespace RomanNumeralKataUnitTest
             Check.That(convertedNumber).IsEqualTo(39);
         }
 
-        //[Fact]
-        //public void return_43_when_the_given_roman_symbol_is_XLIII()
-        //{
-        //    string romanSymbol = "XLIII";
-        //    RomanNumeral romanNumeral = new RomanNumeral();
+        [Fact]
+        public void return_43_when_the_given_roman_symbol_is_XLIII()
+        {
+            string romanSymbol = "XLIII";
+            RomanNumeral romanNumeral = new RomanNumeral();
 
-        //    int convertedNumber = romanNumeral.ConvertToArabicNumber(romanSymbol);
+            int convertedNumber = romanNumeral.ConvertToArabicNumber(romanSymbol);
 
-        //    Check.That(convertedNumber).IsEqualTo(43);
-        //}
+            Check.That(convertedNumber).IsEqualTo(43);
+        }
 
     }
 }
